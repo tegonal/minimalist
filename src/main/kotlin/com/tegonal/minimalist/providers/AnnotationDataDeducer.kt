4168d77aa7254749970f70347b902bd63bbff7b9
Robert Stoll <robert.stoll@tegonal.com>
2025-09-24 12:03:53 +0200

    provide a base implementation for AnnotationDeducer based on annotations

--- src/main/kotlin/com/tegonal/minimalist/providers/AnnotationDataDeducer.kt
+++ src/main/kotlin/com/tegonal/minimalist/providers/AnnotationDataDeducer.kt
@@ -2,6 +2,10 @@ package com.tegonal.minimalist.providers
 
 import java.lang.reflect.Method
 
+/**
+ * Responsible to deduce [AnnotationData] based on a given test [Method] and the args source method name.
+ * @since 2.0.0
+ */
 interface AnnotationDataDeducer {
 	fun deduce(testMethod: Method, argsSourceMethodName: String): AnnotationData?
 }
